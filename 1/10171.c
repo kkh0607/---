0a1e76eaf3f3e1a4d98daf36b8f99f629b4c37ee
kkh0607 <47058506+kkh0607@users.noreply.github.com>
2019-12-29 05:24:11 +0100

    Create 10171.c

---
+++ 1/10171.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+  
+  printf("\\    /\\\n"); // \는 \\로 써야 프린트 가능
+  printf(" )  ( ')\n");  // \n 은 엔터
+  printf("(  /  )\n");
+  printf(" \\(__)|\n");
+  
+  return 0;
+}
