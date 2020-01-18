9da8ffa06f4b1533c055dea8b912c5638ca30ce0
minji Kim <sophiaminji@gmail.com>
2020-01-18 17:29:58 +0100

    commit

--- 8/1011.c
+++ 8/1011.c
@@ -1,6 +1,49 @@
 #include <stdio.h>
 
+long long plus(long long n){
+    return (n*(n+1))/2;
+}
+
+long long steps(long long n){
+    
+    long long distance = 0;
+    
+    if(n < 3) {return n;}
+    
+    if(n%2==0){
+        distance = plus(n/2)*2;
+    }
+    else{
+        distance = plus((n-1)/2)*2+((n-1)/2)+1;
+    }
+    
+    return distance;
+    
+}
+
 int main(){
     
+    long long a, b;
+    long long distance = 0;
+    long long count = 0;
+    
+    int testcase;
+    scanf("%d", &testcase);
+    
+    for(int i = 0; i<testcase; i++){
+        count = 1;
+        scanf("%lld %lld", &a, &b);
+        distance = b - a;
+        
+        while(distance > steps(count)){
+            count++;
+        }
+        
+        printf("%lld\n", count);
+        
+        
+    }
+    
+    return 0;
 }
 
