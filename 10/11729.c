009820844953afcfecec4b3ae38d6ce4017779ee
minji Kim <sophiaminji@gmail.com>
2020-02-03 18:56:44 +0100

    commit

--- 10/11729.c
+++ 10/11729.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
+void hanoi(int n, int first, int second, int third){
+    if(n == 1){
+        printf("%d %d\n", first, third);
+    }
+    else{
+        hanoi(n-1, first, third, second);
+        printf("%d %d\n", first, third);
+        hanoi(n-1, second, first, third);
+    }
+}
+
 int main(){
+    
+    int n;
+    scanf("%d", &n);
+    
+    int total = 1;
+    for(int i = 0; i < n-1; i++){
+        total = total*2 + 1;
+    }
+    
+    printf("%d\n", total);
+    hanoi(n,1,2,3);
+    
     
     return 0;
 }
