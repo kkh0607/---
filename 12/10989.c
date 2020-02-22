ea07b8ead24989f417e323f6e9f209046dc3b320
minji Kim <sophiaminji@gmail.com>
2020-02-22 03:43:48 +0100

    commit

--- 12/10989.c
+++ 12/10989.c
@@ -1,2 +1,23 @@
 #include <stdio.h>
 
+//counting sort when the range of number are small enough
+int main(){
+    
+    int N; int arr[10001] = {0};
+    scanf("%d", &N);
+    
+    int index;
+    for(int i = 0; i < N; i++){
+        scanf("%d", &index);
+        arr[index]++;
+    }
+    
+    for(int i = 0; i < 10001; i++){
+        while(arr[i]){
+            printf("%d\n", i);
+            arr[i]--;
+        }
+    }
+    
+    return 0;
+}
